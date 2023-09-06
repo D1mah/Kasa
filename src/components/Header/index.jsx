7f438ef9fff3721159d77f89a6b9ede121bc0b9b
hamidBehlouli <behlouli.hamid@gmail.com>
2023-09-06 19:07:41 +0200

    header done

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,44 @@
-import '../../utils/style/Header.css'
+import Logo from '../../assets/LOGO.png'
+
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { StyledLink } from '../../utils/style/Bounds'
+
+
+const StyledLogo= styled.img`
+    height:47px;
+    width:145px;
+
+        @media (min-width :768px){
+        height:68px; 
+        width:210px;
+        }
+`
+
+const HeaderNavContainer=styled.nav`
+    padding: 20px 20px 0 20px;
+    display:flex;
+    justify-content: space-between;
+    align-Items: center;
+
+        @media (min-width:768px){
+            padding:20px;
+            text-transform:capitalize;    
+        }
+`
 
 function Header() {
-    return <div className='lmj-banner'>
-        <h1>Kasa</h1>
-    </div>
+    return (<HeaderNavContainer>
+        <Link to='/'>
+          <StyledLogo src={Logo} alt='logo Kasa'></StyledLogo>
+        </Link>
+        <div>
+          <StyledLink to="/">Accueil</StyledLink>
+          <StyledLink to="/about">A Propos</StyledLink>
+          
+        </div>
+      </HeaderNavContainer>
+    )
     
 }
 
